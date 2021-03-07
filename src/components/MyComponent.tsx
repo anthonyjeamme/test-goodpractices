4d77328c2efe2b5bce9b3e6fef7cd6483153e458
anthony jeamme <tonyjahim@gmail.com>
2021-03-07 17:11:22 +0100

    test

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 const MyComponent = ({ data }) => {
 	if (!data) return null
 
-	const a = ''
-
 	return <div className="MyComponent">Ton nom : {data.name}</div>
 }
 export default MyComponent
